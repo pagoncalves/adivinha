fae78e03c142984e8c3ea22771640c4a1d704635
Patricia Pereira <patriciagpereira89@gmail.com>
2021-06-19 10:03:17 -0300

    mais condições

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
+    this.state_inicio = {
       num: Math.floor(Math.random() * 10),
       cont: 3,
       adv: 0,
       ult: 0    
 
     }
+
+    this.state = this.state_inicio
   }
 
   handleInput = (event) => {
@@ -25,13 +27,9 @@ class App extends React.Component {
       cont: this.state.cont - 1
     }))
   }
+
   handlenovo = () => {
-    this.setState ({
-      num: Math.floor(Math.random() * 10),
-      cont: 3,
-      adv: 0,
-      ult: 0,
-    })
+    this.setState(this.state_inicio)
   }
 
   render()
@@ -43,7 +41,7 @@ class App extends React.Component {
       <p>Chute um número entre 0 e 10</p>
       <p>Você tem {this.state.cont} tentativas!</p>
       <input name="adv" value={this.state.adv} onChange={this.handleInput} />
-      {this.state.cont !== 0 && <button onClick={this.handlebtn}>Adivinhei?</button>}
+      {this.state.cont !== 0 && this.state.ult !== this.state.num && <button onClick={this.handlebtn}>Adivinhei?</button>}
       <p>O numéro que você chutou é {this.state.ult} </p>
       {this.state.ult > this.state.num && <p>Muito alto!</p>}
       {this.state.ult < this.state.num && <p>Muito baixo!</p>}
