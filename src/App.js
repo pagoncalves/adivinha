a3a4a7b8120808c08c2b6d0a2bf214f09fa7c7bd
Patricia Pereira <patriciagpereira89@gmail.com>
2021-06-19 09:27:28 -0300

    inclussão de contador

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import './App.css';
-import { render } from 'react-dom';
-
-
 
 class App extends React.Component {
   constructor(props) {
@@ -10,10 +7,9 @@ class App extends React.Component {
 
     this.state = {
       num: Math.floor(Math.random() * 10),
-      cont: 0,
+      cont: 3,
       adv: 0,
-      ult: 0,
-      result: ""
+      ult: 0    
 
     }
   }
@@ -24,34 +20,60 @@ class App extends React.Component {
   }
 
   handlebtn = () => {
-    
-     }
+    this.setState((state) => ({
+      ult: parseInt(state.adv), //guardar o numero digitado
+      cont: this.state.cont - 1
+    }))
+  }
+  handlenovo = () => {
+    this.setState ({
+      num: Math.floor(Math.random() * 10),
+      cont: 3,
+      adv: 0,
+      ult: 0,
+    })
+  }
 
-  render(
+  render()
 
-    if (adv == num){
-      this.setState((state) => ({ 
-        result: `Isso aí!! Você adivinhou o número era ${this.state.num}` }))
-    }else if (adv > num) {
-      this.setState((state) => ({
-       result: `O número ${this.state.adv} que você chutou é um número maior do que eu havia escolhido, tente novamente` }))
-    } else {
-      this.setState ((state) => ({
-        result: `O número ${this.state.adv} que você chutou é um número menor do que eu havia escolhido, tente novamente`}))
-    
-    }
-   {
+    //for (var i = 3;i >= 0;i--) { }
+  /*
 
-    return (
-      <div className="App">
-        <p>O numéro que estou pensando é {this.state.num} </p>
-        menssagem if maior menor chutes
-        <input name="adv" value={this.state.adv} onChange={this.handleInput} />
-        <button onClick={this.handlebtn}></button>
-        <p>{this.state.result}</p>
-      </div>
 
-    )
+    if(adv == num) {
+  this.setState((state) => ({
+    result: `Isso aí!! Você adivinhou o número era ${this.state.num}`
+  }))
+  }else if (adv > num) {
+  this.setState((state) => ({
+    result: `O número ${this.state.adv} que você chutou é um número maior do que eu havia escolhido, tente novamente`
+  }))
+  } else {
+  this.setState((state) => ({
+    result: `O número ${this.state.adv} que você chutou é um número menor do que eu havia escolhido, tente novamente`
+  }))
+
+  }*/
+  {
+  
+  return (
+    <div className="App">
+      <p>Chute um número entre 0 e 10</p>
+      <p>Você tem {this.state.cont} tentativas!</p>
+      <input name="adv" value={this.state.adv} onChange={this.handleInput} />
+      <button onClick={this.handlebtn}>Adivinhei?</button>
+      <p>O numéro que você chutou é {this.state.ult} </p>
+      {this.state.ult > this.state.num && <p>Muito alto!</p>}
+      {this.state.ult < this.state.num && <p>Muito baixo!</p>}
+      {this.state.cont === 0 && <p>Acabaram as chances!</p>}
+      {this.state.ult === this.state.num && <p>Acertou!</p>}
+      
+     <button onClick={this.handlenovo}>Novo Jogo</button>
+      
+      
+    </div>
+
+  )
 
   }
 }
